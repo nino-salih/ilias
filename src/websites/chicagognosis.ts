93f617a2d39e209e186d3fe4edc2d490dd169a37
Nino Salihbegovic <nino@salihbegovic.de>
2023-10-02 22:27:02 +0200

    Added chicagognosis.org

---
+++ src/websites/chicagognosis.ts
@@ -0,0 +1,90 @@
+import axios, { AxiosError } from "axios";
+import { Rules, Webpage, Website } from "./website.js";
+import * as cheerio from "cheerio";
+
+export class Chicagognosis extends Website {
+    skip: { url: RegExp; }[] = [];
+    baseUrl: string = "https://chicagognosis.org/";
+    sitemap: string = "https://chicagognosis.org/sitemap.xml"
+    webpage: Webpage = {
+        author: undefined,
+        title: undefined,
+        book: undefined,
+        content: undefined,
+        source: undefined,
+        course: undefined,
+      };
+
+    private rules: Rules[] = [
+        {
+            // //https://chicagognosis.org/articles/why-is-sexual-magic-controversial
+            url: /https:\/\/chicagognosis\.org\/articles\/.*/,
+            selector: "#wsite-content",
+            remove:
+              ".blog-header,.blog-comments-bottom,.blog-comment-area,.pb-audio-player,#commentArea",
+            title: ".blog-title-link",
+        },
+        {
+            //https://chicagognosis.org/lectures/spiritual-discipline
+            url: /https:\/\/chicagognosis\.org\/lectures\/.*/,
+            selector: "#wsite-content",
+            remove:
+              ".blog-header,.blog-comments-bottom,.blog-comment-area,.pb-audio-player,#commentArea",
+            title: ".blog-title-link",
+        },
+    ];
+
+    async download(url: string): Promise<string> {
+        if (!RegExp(this.baseUrl).exec(url)) {
+            throw new Error("Invalid URL");
+          }
+
+          this.resetInfo();
+      
+          for (const rule of this.rules) {
+            if (rule.url.test(url)) {
+              const webpage = await axios
+                .get(url)
+                .then(({ data }) => {
+                  return data;
+                })
+                .catch((reason: AxiosError) => {
+                  console.log(reason);
+                  throw new Error("Website has thrown an Error");
+                });
+      
+              const $ = cheerio.load(webpage);
+
+              this.webpage.author = "Chicagognosis"
+
+              if(rule.title) {
+                this.webpage.title = $(rule.title).text().trim();
+              }
+
+              if(rule.remove) {
+                $(rule.remove).remove();
+              }
+
+              if(rule.selector) {
+                this.webpage.content = $(rule.selector).text();
+              }
+
+                this.webpage.source = url;
+                return this.webpage.content as string;
+            }
+        }
+        throw new Error("Don't know how to handle this url");
+
+    }
+
+    private resetInfo() {
+        this.webpage = {
+        author: undefined,
+        title: undefined,
+        book: undefined,
+        content: undefined,
+        source: undefined,
+        course: undefined,
+        }
+    }
+}
---
+++ test/websites/chicagognosis.test.ts
@@ -0,0 +1,73 @@
+import {describe, expect, beforeEach, it, jest } from '@jest/globals';
+import axios from 'axios';
+import {Glorian} from '../../src/websites/glorian.js';
+import { fail } from 'assert';
+import { Chicagognosis } from '../../src/websites/chicagognosis.js';
+
+describe('Chicagognosis', () => {
+    let chicagognosis: Chicagognosis = new Chicagognosis();
+  
+    describe("download", () => {
+        it("should throw an error for invalid URL", async () => {
+          await expect(chicagognosis.download("https://example.com")).rejects.toThrow(
+            "Invalid URL"
+          );
+        });
+
+        it('should throw an error if the website returns an HTTP 404 error', async () => {
+          const mock = jest.spyOn(axios, 'get');
+          mock.mockReturnValueOnce(Promise.reject({ status: 404 }));
+
+          await expect(chicagognosis.download('https://chicagognosis.org/lectures/freedom-in-times-of-suffering')).rejects.toThrow('Website has thrown an Error');
+
+          mock.mockRestore();
+        });
+
+        it("should throw an error for not parseble URL", async () => {
+          await expect(
+            chicagognosis.download("https://chicagognosis.org/index.html")
+          ).rejects.toThrow("Don't know how to handle this url");
+        });
+
+        //https://chicagognosis.org/articles/why-is-sexual-magic-controversial
+        it("should download an article", async () => {
+            const content = await chicagognosis.download(
+                "https://chicagognosis.org/articles/why-is-sexual-magic-controversial"
+              );
+              expect(content).toContain("The verse from 1 Peter 2:8 also states: \"They stumble because");
+              expect(chicagognosis.webpage.title).toBe("Why Is Sexual Magic Controversial?");
+              expect(chicagognosis.webpage.author).toBe("Chicagognosis");
+            });
+        
+        it("Content on artical should not have", async () => {
+            const content = await chicagognosis.download(
+                "https://chicagognosis.org/articles/why-is-sexual-magic-controversial"
+            );
+            expect(content).not.toContain("Why Is Sexual Magic Controversial?");
+            expect(content).not.toContain("2/10/2021");
+            expect(content).not.toContain("Your comment will be posted after it is approved.");
+            });
+        });
+        //https://chicagognosis.org/lectures/spiritual-discipline
+        it("should download a lecture", async () => {
+            const content = await chicagognosis.download(
+                "https://chicagognosis.org/lectures/spiritual-discipline"
+              );
+              expect(content).toContain("We were explaining this teaching in relation to the mysticism of Islam, and Islam in Arabic means submission to God's will");
+              expect(chicagognosis.webpage.title).toBe("Spiritual Discipline");
+              expect(content).not.toContain("Chicago Gnosis Podcast");
+              expect(chicagognosis.webpage.author).toBe("Chicagognosis");
+            });
+
+        //https://chicagognosis.org/lectures/spiritual-discipline
+        it("Content on lectures should not have", async () => {
+            const content = await chicagognosis.download(
+                "https://chicagognosis.org/lectures/spiritual-discipline"
+            );
+            expect(content).not.toContain("Spiritual Discipline");
+            expect(content).not.toContain("8/27/2016");
+            expect(content).not.toContain("Chicago Gnosis Podcast");
+            expect(content).not.toContain("Your comment will be posted after it is approved.");
+            });
+        
+  }); 
